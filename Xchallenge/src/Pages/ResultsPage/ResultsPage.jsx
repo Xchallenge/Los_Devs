97c669d8d3d3940887306795e02c124d1f71d5d1
Richard Fernandes Passarelli <45811737874@info.local>
2023-12-18 14:56:24 -0300

    Finalizado estrutura do react

--- Xchallenge/src/Pages/ResultsPage/ResultsPage.jsx
+++ Xchallenge/src/Pages/ResultsPage/ResultsPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./ResultsPage.css";
+import TableRe from './TableRe/TableRe';
 
 const ResultsPage = () => {
     return (
         <div>
+            <TableRe
             
+                
+            
+            />
         </div>
     );
 };
---
+++ Xchallenge/src/Pages/ResultsPage/TableRe/TableRe.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const TableRe = () => {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Medalha</th>
+                        <th>Competidor</th>
+                        <th>Resultado</th>
+                        <th>Pais</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>01</td>
+                        <td>Ouro</td>
+                        <td>Gustavo</td>
+                        <td>900</td>
+                        <td>Brazil</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default TableRe;
